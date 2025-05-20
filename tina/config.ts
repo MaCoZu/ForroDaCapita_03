98f90c0c2777b96661c5b669f8c130adec3a51d4
MaCoZu <marcoz@posteo.de>
2025-05-20 15:57:26 +0200

    fyi

--- tina/config.ts
+++ tina/config.ts
@@ -10,7 +10,7 @@ const branch =
 
 export default defineConfig({
   token: process.env.TINA_TOKEN, // This should match the value in your .env file
-  clientId: process.env.TINA_CLIENT_ID, // This should match the value in your .env file
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID, 
   branch,
 
   build: {
