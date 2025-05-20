5098c5b8856609bb60d901def1e11346b5b1f360
MaCoZu <marcoz@posteo.de>
2025-05-20 14:50:17 +0200

    cloud

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.forrodacapita.de',
+  site: 'https://forrodacapita.netlify.app',
   integrations: [mdx(), sitemap(), react(), alpinejs()],
 
   build: {
