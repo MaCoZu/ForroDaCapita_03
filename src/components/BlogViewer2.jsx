38f07ad4736649b1abb20d9497081e5f13deebbc
MaCoZu <marcoz@posteo.de>
2025-05-22 10:48:03 +0200

    update time, remark gfm

--- src/components/BlogViewer2.jsx
+++ src/components/BlogViewer2.jsx
@@ -1,46 +1,62 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm';
 
 export default function BlogViewer({ posts, postContents }) {
     const [currentSlug, setCurrentSlug] = useState(posts[0].id)
     const currentPost = posts.find((post) => post.id === currentSlug)
     const markdown = postContents[currentPost.id]
 
+    // Helper function to format the date and time
+    const formatDateTimeString = (dateString) => {
+        const displayDate = new Date(dateString);
+        return displayDate
+            .toLocaleDateString("de-DE", {
+                timeZone: "Europe/Berlin",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+                hour: "2-digit",
+                minute: "2-digit",
+            })
+            .replace(" um", " |");
+    };
+
+
     return (
         <>
             <aside className="lg:col-span-1 overflow-y-auto">
                 <ul className="space-y-1 pr-4">
-                    {posts.map((post) => (
-                        <li key={post.id}>
-                            <button
-                                className={`block w-full font-merri text-left my-1 p-4 rounded-lg hover:bg-amber-100 transition-colors ${currentSlug === post.id ? 'bg-amber-100' : 'bg-amber-50'
-                                    }`}
-                                onClick={() => setCurrentSlug(post.id)}
-                            >
-                                <time className="text-lg text-gray-500">
-                                    {new Date(post.pubDate).toLocaleDateString('de-DE', {
-                                        year: 'numeric',
-                                        month: 'long',
-                                        day: 'numeric',
-                                    })}
-                                </time>
+                    {posts.map((post) => {
+                        const listItemDate = post.updatedDate || post.pubDate;
 
-                                <h3 className="font-bold">{post.title}</h3>
-                            </button>
-                        </li>
-                    ))}
+                        return (
+                            <li key={post.id}>
+                                <button
+                                    className={`block w-full font-merri text-left my-1 p-4 rounded-lg hover:bg-amber-100 transition-colors ${currentSlug === post.id ? 'bg-amber-100' : 'bg-amber-50'
+                                        }`}
+                                    onClick={() => setCurrentSlug(post.id)}
+                                >
+                                    <time className="text-sm text-gray-500">
+                                        {post.updatedDate ? "updated: " : ""}
+                                        {formatDateTimeString(listItemDate)}
+                                    </time>
+                                    <h3 className="text-xl font-bold mt-2">
+                                        {post.title}
+                                    </h3>
+                                </button>
+                            </li>
+                        );
+                    })}
                 </ul>
             </aside>
 
             <main className="lg:col-span-2 overflow-y-auto">
                 {currentPost && (
                     <article className="prose prose-stone font-merri max-w-none p-6">
-                        <time className="text-lg text-gray-500">
-                            {new Date(currentPost.pubDate).toLocaleDateString('de-DE', {
-                                year: 'numeric',
-                                month: 'long',
-                                day: 'numeric',
-                            })}
+                        <time className="text-lg flex justify-end md:pr-7 text-md text-gray-500">
+                            {currentPost.updatedDate ? "updated: " : ""}
+                            {formatDateTimeString(currentPost.updatedDate || currentPost.pubDate)}
                         </time>
                         <h1 className="prose-stone font-merri font-semibold text-2xl mb-6">
                             {currentPost.title}
@@ -56,7 +72,7 @@ export default function BlogViewer({ posts, postContents }) {
                                         prose-img:rounded-xl
                                 "
                         >
-                            <ReactMarkdown>
+                            <ReactMarkdown remarkPlugins={[remarkGfm]} >
                                 {markdown}
                             </ReactMarkdown>
                         </div>
